8b02b3fe46de2a65998042afb3e6a88fdcf8c41d
Alexander Todorov <alext@Alexanders-MacBook-Pro.local>
2018-09-12 12:13:04 +0300

    Added basic webpage

--- scripts/install_nginx.sh
+++ scripts/install_nginx.sh
@@ -5,3 +5,14 @@ which ${PKG}  || {
    apt-get update -y 
     apt-get install -y ${PKG}
 } 
+
+#to make sure we are where our files are like travis
+[ -d /vagrant ] && pushd /vagrant
+
+#use our conf file
+[ -f /etc/nginx/sites-enabled/default ] && rm /etc/nginx/sites-enabled/default
+cp nginx.conf /etc/nginx/sites-enabled/default
+service nginx restart
+
+#deploy our webpage
+cp index.html /var/www/html/
